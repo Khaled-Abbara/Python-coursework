d41274d32cefc4b66abfbc2939063895568307f1
Khaled Abbara <148062226+Khaled-Abbara@users.noreply.github.com>
2025-09-13 17:55:39 +0800

    submit

---
+++ programming-assignments/01-assignment.py
@@ -0,0 +1,62 @@
+# a. If you are trying to print your name, 
+# what happens if you leave out one of the quotation marks or both, and why? 
+
+# print(Mohamad Alkhaled Abara)
+
+
+
+# b. What is the difference between * and ** operators in Python?
+# Explain with the help of an example. 
+
+# print(5 * 3)
+# print(5 ** 3)
+
+
+
+# c. In Python, is it possible to display an integer like 09? Justify your answer.
+
+# print(09)
+
+
+
+# d. Run the commands type('67') and type(67). What is the difference in the output and why? 
+
+# print(type('67'))
+# print(type(67))
+
+
+
+
+# a. To multiply your age by 2 and display it.  For example, if your age is 16, so 16 * 2 = 32 
+
+# def multiply_age(age):
+#     print(age * 2)
+
+# multiply_age(19)
+
+
+
+# b. Display the name of the city, country, and continent you are living in.
+
+# city = "Kuala Lumpur"
+# country = "Malaysia"
+# continent = "Asia"
+# print("City:", city)
+# print("Country:", country)
+# print("Continent:", continent)
+
+
+
+# c. To display the examination schedule (i.e., the starting and the ending day) of this term.
+
+# starting_day = "June 19, 2025"
+# ending_day = "August 20, 2025"
+# print("Starting Day:", starting_day)
+# print("Ending Day:", ending_day)
+
+
+
+# d. Display the temperature of your country on the day the assignment is attempted by you  
+
+# Temperature = 27
+# print("Today's Temperature is ", Temperature)
